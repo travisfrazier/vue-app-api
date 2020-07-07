a262e6c8d12ed9402c7bd7be398dc8660c7ab37a
Travis Frazier <travisfrazier@traviss-mbp.attlocal.net>
2020-07-07 15:17:11 -0400

    first commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,20 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from '~/components/Header.vue';
+
+export default {
+  components: {
+    Header
+  },
+  methods: {
+   
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
